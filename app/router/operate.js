fb905a9534d1c69cd26181c7c407636f56c140ec
joker <215537936@qq.com>
2020-11-17 23:39:34 +0800

    新增：权限列表条件筛选

--- app/router/operate.js
+++ app/router/operate.js
@@ -7,5 +7,7 @@ module.exports = app => {
   const { router, controller, middleware } = app;
   const jwt = middleware.tokenHandler(app.config.jwt);
   router.get('/operate/getUserList', jwt, controller.operate.queryUserListByAdminId);
+  router.get('/operate/getRoleList', jwt, controller.operate.queryRoleListByAdminId);
+  router.post('/operate/deleRoleById', jwt, controller.operate.deleteRoleById);
 };
 
--- app/service/operate.js
+++ app/service/operate.js
@@ -9,7 +9,6 @@ class OperateService extends Service {
     const { admin_id = '' } = await ctx.service.common.getUserInfo();
     const { page = {}, column = {} } = option;
     const { pageSize = 10, current = 0 } = page;
-    console.log({ admin_id, ...column });
     const result = await app.mysql.select('admin_userinfo', {
       where: { admin_id, ...column }, // WHERE 条件
       orders: [['id', 'desc']], // 排序方式
@@ -24,6 +23,29 @@ class OperateService extends Service {
       current,
       pageSize
     };
+  }
+
+  // 根据店铺身份返回店铺下面的所有权限列表
+  async queryRoleListByAdminId() {
+    const { ctx, app } = this;
+    const { admin_id = '' } = await ctx.service.common.getUserInfo();
+    return await app.mysql.select('admin_role', {
+      columns: ['id', 'name', 'create_time', 'status'],
+      where: { admin_id }, // WHERE 条件
+      orders: [['id', 'desc']], // 排序方式
+    });
+  }
+
+  // 根据权限id返回权限内容
+  async queryRoleInfoByRoleId(id) {
+    const { app } = this;
+    return await app.mysql.get('admin_role', { id });
+  }
+
+  // 删除权限
+  async deleteRoleById(id) {
+    const { app } = this;
+    return await app.mysql.delete('admin_role', { id });
   }
 }
 
